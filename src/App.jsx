cf361267995f8f3fee258da2f6019a263d9431a1
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-03-17 12:13:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Blog from './components/Blogs/Blog'
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Blog></Blog>
+    </>
+  )
+}
+
+export default App
